511f433b997d5fc6ecec68ff97f4c3aae06a5035
Julius Fidelis Hubilla <kuramamode11@gmail.com>
2018-02-18 16:06:10 +0800

    update nav

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -5,6 +5,7 @@
         <img src="{{ asset('img/logo.png') }}" alt="Logo" class="img-icon d-inline-block align-top">
         {{ config('app.name') }}
     </a>
+    
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-target" aria-controls="nav-target" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -12,18 +13,21 @@
     <div class="collapse navbar-collapse justify-content-end" id="nav-target">
       <ul class="navbar-nav">
         @if (Auth::guest())
-            <li class="nav-item"><a href="#features" class="nav-link">Features</a></li>
-            <li class="nav-item"><a href="#about" class="nav-link">About</a></li>
+          <li class="nav-item"><a href="#features" class="nav-link">Features</a></li>
+          <li class="nav-item"><a href="#about" class="nav-link">About</a></li>
         @else
+          <li class="nav-item"><a href="" class="nav-link"><i class="fa fa-files-o"></i> Manage</a></li>
+          <li class="nav-item"><a href="" class="nav-link"><i class="fa fa-file"></i> Records</a></li>
           <li class="nav-item dropdown">
-
             <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
                aria-haspopup="true" aria-expanded="false">
                 {{ Auth::user()->name }}
             </a>
             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+              <a href="" class="dropdown-item"><i class="fa fa-cog"></i> Settings</a>
               <a href="{{ route('logout') }}" class="dropdown-item" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                  Logout
+                <i class="fa fa-sign-out"></i>
+                Logout
               </a>
 
               <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
